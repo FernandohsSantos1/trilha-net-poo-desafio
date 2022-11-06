dc2d633d0dd9ed5e007e7275ca9d99cafed2a2c7
Fernando.Santos <fernando.santos@obraprimaweb.com.br>
2022-11-06 14:23:44 -0300

    Commit projeto pronto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iniciando programa...\n");
+
+//Instânciando o iphone e o nokia:
+Iphone celularIphone = new Iphone(numero: "3333-4444", modelo: "3GS", imei: "1234", memoria: 512);
+Nokia celularNokia = new Nokia(numero: "4444-5555", modelo: "C01 Plus", imei: "4321", memoria: 512);
+
+//Chamando seus respectivos métodos:
+Console.WriteLine("Celular Iphone:");
+celularIphone.Ligar();
+celularIphone.ReceberLigacao();
+celularIphone.InstalarAplicativo("Discord");
+
+Console.WriteLine("\nCelular Nokia:");
+celularNokia.Ligar();
+celularNokia.ReceberLigacao();
+celularNokia.InstalarAplicativo("WhatsApp");
